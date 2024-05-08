aec192ca1e67accb0622f182b5abfaa72e7ef6bc
earlyou <earlyou1@gmail.com>
2024-05-08 15:41:29 +0900

    first commit

---
+++ src/main/java/com/earlyou/catbudget/CatbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.earlyou.catbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/earlyou/catbudget/CatbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earlyou.catbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
