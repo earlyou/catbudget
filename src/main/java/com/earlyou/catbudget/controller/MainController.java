9b1d575aac94c34aeea95489745fe755d5a9fd13
earlyou <earlyou1@gmail.com>
2024-05-08 16:52:17 +0900

    testing bootstrap templates

--- src/main/java/com/earlyou/catbudget/controller/MainController.java
+++ src/main/java/com/earlyou/catbudget/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.earlyou.catbudget.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String main() {
+	public String main(Model m) {
+		m.addAttribute("sidebar", "sidebar");
+		m.addAttribute("main", "main/main");
 		return "index";
 	}
 }
