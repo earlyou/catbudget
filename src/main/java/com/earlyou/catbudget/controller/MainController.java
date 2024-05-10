7e3585182a768676687eb04e6cb3d83439772fad
earlyou <earlyou1@gmail.com>
2024-05-10 15:53:13 +0900

    testing templates

--- src/main/java/com/earlyou/catbudget/controller/MainController.java
+++ src/main/java/com/earlyou/catbudget/controller/MainController.java
@@ -13,4 +13,25 @@ public class MainController {
 		m.addAttribute("main", "main/main");
 		return "index";
 	}
+	
+	@GetMapping("/login")
+	public String login(Model m) {
+		m.addAttribute("sidebar", "sidebar");
+		m.addAttribute("main", "auth/login");
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String register(Model m) {
+		m.addAttribute("sidebar", "sidebar");
+		m.addAttribute("main", "auth/register");
+		return "index";
+	}
+	
+	@GetMapping("/forgot")
+	public String forgot(Model m) {
+		m.addAttribute("sidebar", "sidebar");
+		m.addAttribute("main", "auth/forgot");
+		return "index";
+	}
 }
